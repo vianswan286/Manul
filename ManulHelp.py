0ac4fa68582cb1556892057c8734d0774f7cf34e
vianswan286 <41406948+vianswan286@users.noreply.github.com>
2023-07-28 15:10:35 +0300

    Update ManulHelp.py

--- ManulHelp.py
+++ ManulHelp.py
@@ -2,7 +2,7 @@ from pyrogram import *
 app = []
 messages = []
 max = 30528
-config = open("config.txt", encoding='utf8')
+config = open("config.txt", encoding='utf8') # Программе требуется файл config.txt, сожержащий Имя аккаунта, его api_id и hash_api
 chat = "manuls_LPR"
 for i in config:
     j = i.rstrip()
@@ -14,11 +14,10 @@ for i in config:
 app[2].start()
 for msg in app[2].get_chat_history(chat):
     messages.append(msg)
-num = set()
+num = set()                                    #Все числа, которые мы ожидаем увидеть
 for j in range(0, max + 1):
     num.add(j)
-
-
+    
 integers = set()
 for soob in messages:
     soo = str(soob.text)
@@ -27,13 +26,13 @@ for soob in messages:
     while m < length:
         s_int = ''  # строка для нового числа
         separators = [' ', ',', '.', "'", '_', '!']
-        while m < length and ('0' <= soo[m] <= '9' or soo[m] in separators):
+        while m < length and ('0' <= soo[m] <= '9' or soo[m] in separators):   #Не все числа записаны в одном нормальном виде 
             if not (soo[m] in separators):
                 s_int += soo[m]
             m += 1
         m += 1
         if s_int != '':
-            integers.add(int(s_int))
+            integers.add(int(s_int))       #Числа, которые мы увидели в чате
 
 lack = num - integers
 message = "Не хватает следующих количеств:\n"
